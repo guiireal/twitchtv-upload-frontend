e9f0075db5fa676cf8eedc9d495a7d7c505ca262
guiireal <guii_real@hotmail.com>
2020-11-29 19:39:50 -0300

    Config storybook

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #f25a80;
+  font-size: 40px;
+  text-shadow: 5px 5px 0px #000;
+  font-family: "Press Start 2P", sans-serif;
+`;
+
+export default Title;
---
+++ src/stories/components/Title.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../components/Title";
+
+const parameters = {
+  title: "Components/Title",
+  component: Title,
+  properties: {
+    layout: "centered",
+  },
+};
+
+export default parameters;
+
+export const Default = () => <Title>Profile</Title>;
